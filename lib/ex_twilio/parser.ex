2044ff489387548ae4260b2aecce679eb5191f49
Derek Rush <drush81@gmail.com>
2020-03-22 16:39:30 -0700

    Update parser.ex

--- lib/ex_twilio/parser.ex
+++ lib/ex_twilio/parser.ex
@@ -42,6 +42,7 @@ defmodule ExTwilio.Parser do
   """
   @spec parse(HTTPoison.Response.t(), module) :: success | error
   def parse(response, module) do
+  IO.inspect(response, label: "RESP")
     handle_errors(response, fn body ->
       Poison.decode!(body, as: target(module))
     end)
